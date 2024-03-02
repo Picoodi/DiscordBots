200bf55897ecf4c722667fd24f7dd3ad283ffaee
Max <161768135+Picoodi@users.noreply.github.com>
2024-03-02 22:17:09 +0100

    Create Helper_Bot.py

---
+++ Helper_Bot.py
@@ -0,0 +1,2 @@
+#This is the code for my Helper-Bot. I use it as a bridge between the users and Mods on my Server. So the users can write report messages or help messages to the bot,
+#who will help them directly or send theyr requests to mods or server owners.
